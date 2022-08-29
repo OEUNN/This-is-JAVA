f758d9d53741437165116b8a06a3c4f5d66ab291
OEUNN <112099013+OEUNN@users.noreply.github.com>
2022-08-29 19:57:49 +0900

    for문
    Scanner와 for문을 활용한 easy

---
+++ JANG/codingtest01.java
@@ -0,0 +1,14 @@
+package practice;
+
+import java.util.Scanner;
+
+public class codingtest01 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int input=Integer.parseInt(sc.nextLine());
+		for(int i=0;i<input;i++) {
+			System.out.println("Hello MyCompany!");
+		}		
+	}
+}
