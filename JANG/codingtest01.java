e0d4a1c26be5b84efbf12b145e32300f4fa921c0
OEUNN <112099013+OEUNN@users.noreply.github.com>
2022-08-31 08:57:09 +0900

    '%'연산자를 사용할수 있는가?
    배열과 연산자를 활용할수 있는지에 대한 문제이다.

--- JANG/codingtest01.java
+++ JANG/codingtest01.java
@@ -1,14 +1,45 @@
-package practice;
+package practice0830;
 
 import java.util.Scanner;
+
+//시작수와 끝수를 입력 받고 주어진 배수의 합을 출력
+
+//[입력]
+//시작수: 1
+//끝수: 15
+//배수: 7
+
+//[출력]
+//21
 
 public class codingtest01 {
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int input=Integer.parseInt(sc.nextLine());
-		for(int i=0;i<input;i++) {
-			System.out.println("Hello MyCompany!");
-		}		
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("시작수 :");
+		int start=Integer.parseInt(sc.nextLine());
+		System.out.print("끝수 :");
+		int end=Integer.parseInt(sc.nextLine());
+		
+		int[] arr=new int[end];
+		int j=0;
+		
+		for(int i=start;i<=end;i++) {
+			arr[j]=i;
+			j++;
+		}
+		
+		System.out.print("배수 :");
+		int num=Integer.parseInt(sc.nextLine());
+		int sum=0;
+		for(int i=0;i<=end-start;i++) {
+			if(arr[i]%num==0) {
+				sum+=arr[i];			
+			}
+		}
+		System.out.println(sum);
+	
 	}
+
 }
