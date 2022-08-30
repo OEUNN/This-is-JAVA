0764f2d8ce366fd13e8db2bed7e336e0a1112069
OEUNN <112099013+OEUNN@users.noreply.github.com>
2022-08-31 08:58:49 +0900

    .charAt( )
    메소드 .charAt() 활용에 대한 문제이다.
    아스키코드는 JAVA에서 char형 변수로 가능하다는 것이다. 
    아스키코드의 값을 받아야하기 때문에 배열도 char형으로 선언 하였다.

--- JANG/codingtest02.java
+++ JANG/codingtest02.java
@@ -1,51 +1,26 @@
-package practice;
-//7개의 자연수가 주어질때, 이들 중 홀수인 자연수들을 모두 골라 그 합을 출력하고, 고른 홀수들 중 최솟값을 출력하는 프로그램을 작성하시오.
-//만약 홀수가 존재하지 않는 경우에는 첫째 줄에 -1츨 출력한다.
+package practice0830;
 
 import java.util.Scanner;
 
 public class codingtest02 {
-
 	public static void main(String[] args) {
-
+		
 		Scanner sc=new Scanner(System.in);
-		String input= sc.nextLine();
-		String[] cut=input.split(",");
-		int[] arr = new int [7];
-		int sum=0; 
-		int min=0;
+		String word=sc.nextLine();
+		char[] password=new char[word.length()];//배열을 char로 받는 것이 중요하다.
 		
-		for(int i=0;i<7;i++) {
-			arr[i]= Integer.parseInt(cut[i]);// ","로 입력값을 자를수 있다.
-		}
-		
-		for(int i=0;i<7;i++) {
-			if((arr[i]%2)==0) {
-				arr[i]=0;
+		for(int i=0;i<password.length;i++) {
+			password[i]=word.charAt(i);//메소드 charAt()를 사용해서 char형으로 배열에 저장
+			if(password[i]>'v') {
+				password[i]-=22;
 			}
-			sum+=arr[i];
-		}	
-		
-		if(sum==0) {
-			System.out.println("-1");
-		}
-		else {
-			sum=0;
-			for(int i=0;i<7;i++) {
-				if((arr[i]%2)!=0) {
-					sum+=arr[i];
-					if(min>arr[i]) {
-						min=arr[i];
-					}else if(min==0) {
-						min=arr[i];
-					}
-				}
-			}
-			System.out.println("최종값은\n"+sum);
-			System.out.println("홀수중 최솟값\n"+min);
 		}
 		
+		for(int i=0;i<password.length;i++) {
+			System.out.print(password[i]);
+		}
 		
 	}
-
 }
+
+		
