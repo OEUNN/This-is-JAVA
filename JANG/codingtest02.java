f6714497671029fc7c2bd1c0e6932f0abc10586b
OEUNN <112099013+OEUNN@users.noreply.github.com>
2022-09-01 13:33:14 +0900

    reapce( ) 메소드

--- JANG/codingtest02.java
+++ JANG/codingtest02.java
@@ -1,26 +1,25 @@
-package practice0830;
+package practice0831;
 
 import java.util.Scanner;
 
 public class codingtest02 {
-	public static void main(String[] args) {
-		
-		Scanner sc=new Scanner(System.in);
-		String word=sc.nextLine();
-		char[] password=new char[word.length()];//배열을 char로 받는 것이 중요하다.
-		
-		for(int i=0;i<password.length;i++) {
-			password[i]=word.charAt(i);//메소드 charAt()를 사용해서 char형으로 배열에 저장
-			if(password[i]>'v') {
-				password[i]-=22;
-			}
-		}
-		
-		for(int i=0;i<password.length;i++) {
-			System.out.print(password[i]);
-		}
-		
-	}
-}
 
-		
+	public static void main(String[] args) {
+		//숫자 문자열과 영단어
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		//s.length()는 1이상 50이하
+		s=s.replace("zero","0");
+		s=s.replace("one","1");
+		s=s.replace("two","2");
+		s=s.replace("three","3");
+		s=s.replace("four","4");
+		s=s.replace("five","5");
+		s=s.replace("six","6");
+		s=s.replace("seven","7");
+		s=s.replace("eight","8");
+		s=s.replace("nine","9");
+		System.out.println(s);
+	}
+
+}
