691d43abf16efa8bbe457fb0de338a475846422d
OEUNN <112099013+OEUNN@users.noreply.github.com>
2022-09-01 13:30:52 +0900

    등산하기
    ((#(())#(()))) 입력을 통해 "("이면 한칸위로 #시작하면 급경사가 시작되는것이다.
    #은 꼭 닫혀야 한다.

--- JANG/codingtest01.java
+++ JANG/codingtest01.java
@@ -1,45 +1,37 @@
-package practice0830;
+package practice0831;
+//태희code
+//((#(())#(())))
 
 import java.util.Scanner;
-
-//시작수와 끝수를 입력 받고 주어진 배수의 합을 출력
-
-//[입력]
-//시작수: 1
-//끝수: 15
-//배수: 7
-
-//[출력]
-//21
 
 public class codingtest01 {
 
 	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String[] mount=sc.nextLine().split("");
 		
-		Scanner sc = new Scanner(System.in);
-		System.out.print("시작수 :");
-		int start=Integer.parseInt(sc.nextLine());
-		System.out.print("끝수 :");
-		int end=Integer.parseInt(sc.nextLine());
-		
-		int[] arr=new int[end];
-		int j=0;
-		
-		for(int i=start;i<=end;i++) {
-			arr[j]=i;
-			j++;
-		}
-		
-		System.out.print("배수 :");
-		int num=Integer.parseInt(sc.nextLine());
+		int high=0;
 		int sum=0;
-		for(int i=0;i<=end-start;i++) {
-			if(arr[i]%num==0) {
-				sum+=arr[i];			
+		int slope=1;
+		boolean choice=true;
+		
+		for(int i=0;i<mount.length;i++) {
+			if(mount[i].equals("(")) {
+				high++;
+				sum+=high*slope;
+			}else if(mount[i].equals(")")){
+				sum+=high*slope;
+				high--;
+			}else if(mount[i].equals("#")){
+				if(choice==false) {
+					slope=2;
+					choice=true;
+				}else
+					slope=1;
+					choice=false;
 			}
 		}
-		System.out.println(sum);
-	
+		 System.out.println(sum);
 	}
 
 }
