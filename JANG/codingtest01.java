1b418c1f030dd00300a0a2767a42e57a8019e245
OEUNN <112099013+OEUNN@users.noreply.github.com>
2022-09-05 20:20:55 +0900

    완주못한선수 찾기
    프로그래머스 문제

--- JANG/codingtest01.java
+++ JANG/codingtest01.java
@@ -1,37 +1,27 @@
-package practice0831;
-//태희code
-//((#(())#(())))
+package practice0901;
 
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class codingtest01 {
 
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		String[] mount=sc.nextLine().split("");
-		
-		int high=0;
-		int sum=0;
-		int slope=1;
-		boolean choice=true;
-		
-		for(int i=0;i<mount.length;i++) {
-			if(mount[i].equals("(")) {
-				high++;
-				sum+=high*slope;
-			}else if(mount[i].equals(")")){
-				sum+=high*slope;
-				high--;
-			}else if(mount[i].equals("#")){
-				if(choice==false) {
-					slope=2;
-					choice=true;
-				}else
-					slope=1;
-					choice=false;
-			}
-		}
-		 System.out.println(sum);
-	}
+   public static void main(String[] args) {
+      String[] participant = { "mislav", "stanko", "mislav", "ana" };
+      Arrays.sort(participant);
 
+      String[] completion = { "stanko", "ana", "mislav" };
+      Arrays.sort(completion);
+      int pLength = participant.length;
+      int cLength = completion.length;
+      String answer = "";
+
+      for (int i = 0; i < completion.length; i++) {
+         if (!participant[i].equals(completion[i])) {
+            answer = participant[i];
+            break;
+         } 
+         answer = participant[completion.length];
+      }
+         System.out.println(answer);
+      
+   }
 }
