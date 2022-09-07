cdbbcde227c6c026ab55d210ffe6a4719c0cacd5
OEUNN <112099013+OEUNN@users.noreply.github.com>
2022-09-07 19:17:36 +0900

    행렬의 덧셈
    2차원수의 자리는 1차원수 자리의 length()이다.

--- JANG/Test01.java
+++ JANG/Test01.java
@@ -1,16 +1,23 @@
-package practice0906.exam01;
+package practice.test0907;
+//행렬의 덧셈
 
 public class Test01 {
+
 	public static void main(String[] args) {
-		int[] absolutes= {4,7,12};
-		boolean[] signs= {true, false, true};
-		int sum=0;
-		for(int i=0;i<absolutes.length;i++) {
-			if(signs[i]==false) {
-				absolutes[i]*=-1;
+		// TODO Auto-generated method stub
+		int[][] arr1= {{1,2},{2,3}};
+		int[][] arr2= {{3,4},{5,6}};
+		int[][] answer= {};
+		
+		for(int i=0;i<arr1.length;i++) {
+			//arr1의 0자리의 길이가 2차원수 배열의 길이 이므로 자리를 지정해서
+			for(int j=0;j<arr1[0].length;j++) {  
+				answer[i][j]=arr1[i][j]+arr2[i][j];
+				System.out.println(answer[i][j]);
 			}
-			sum+=absolutes[i];
 		}
-		System.out.println(sum);
+		
+		
 	}
+
 }
