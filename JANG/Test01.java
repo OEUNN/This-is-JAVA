52283142acab1135d13d2d4633d9701ca3937f79
OEUNN <112099013+OEUNN@users.noreply.github.com>
2022-09-08 16:32:17 +0900

    프로그래머스 제곱근 수 구하기

--- JANG/Test01.java
+++ JANG/Test01.java
@@ -1,23 +1,22 @@
-package practice.test0907;
-//행렬의 덧셈
+package practice.test0908;
+
 
 public class Test01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[][] arr1= {{1,2},{2,3}};
-		int[][] arr2= {{3,4},{5,6}};
-		int[][] answer= {};
+		long n=121;
+		long answer=0;
 		
-		for(int i=0;i<arr1.length;i++) {
-			//arr1의 0자리의 길이가 2차원수 배열의 길이 이므로 자리를 지정해서
-			for(int j=0;j<arr1[0].length;j++) {  
-				answer[i][j]=arr1[i][j]+arr2[i][j];
-				System.out.println(answer[i][j]);
+		for(int i=1;i<=n;i++) {
+			if(i*i==n) {
+				answer=i;
+				answer=(answer+1)*(answer+1);
+				break;
+			}else {
+				answer=-1;
 			}
 		}
-		
-		
+		System.out.println(answer);
 	}
 
 }
